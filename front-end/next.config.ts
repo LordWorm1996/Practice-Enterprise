175633d1601e00bbcb0a605659c958d8a02e7c5d
Dimitrios Psaroudakis <dimitrios.psaroudakis@gmail.com>
2025-03-08 13:20:05 +0100

    made node.js run the front end

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -1,7 +1,14 @@
+/** @type {import('next').NextConfig} */
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+//const nextConfig: NextConfig = {
+/* config options here */
+//};
+
+const nextConfig = {
+  output: "export",
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
